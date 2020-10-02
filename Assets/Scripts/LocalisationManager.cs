c0c420c246770046ada9c72e88a41a8a2da08f68
Dmitry Mozgin <m0391n@gmail.com>
2020-10-02 17:37:51 +0300

    Basic Setup

---
+++ Assets/Scripts/LocalisationManager.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using m039.Common;
+
+namespace Sokoban
+{
+	public static class LocalisationManager
+	{
+		static SimpleLocalisationSystem __Instance;
+
+		static SimpleLocalisationSystem _Instance
+		{
+			get
+			{
+				if (__Instance == null)
+				{
+					__Instance = new SimpleLocalisationSystem();
+					__Instance.LoadData();
+
+					// Take a default language from the prefs and set it.
+					var currentLanguage = StorageSystem.Settings.GetSelectedLanguage();
+					if (currentLanguage != null)
+					{
+						if (__Instance.Languages.IndexOf(currentLanguage) != -1)
+						{
+							__Instance.CurrentLanguage = currentLanguage;
+						}
+					}
+				}
+
+				return __Instance;
+			}
+		}
+
+		public static List<string> GetLanguages()
+		{
+			return _Instance.Languages;
+		}
+
+		public static string GetCurrentLanguage()
+		{
+			return _Instance.CurrentLanguage;
+		}
+
+		public static void SetLanguage(string language)
+		{
+			_Instance.CurrentLanguage = language;
+		}
+
+		public static string GetString(string key)
+		{
+			return _Instance[key];
+		}
+
+		public static void SelectNextLanguage()
+		{
+			_Instance.SelectNextLanguage();
+		}
+
+		public static void SelectPrevLanguage()
+		{
+			_Instance.SelectPrevLanguage();
+		}
+	}
+}
---
+++ Assets/Scripts/WelcomeScreen.cs
@@ -0,0 +1,23 @@
+﻿using m039.Common;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WelcomeScreen : MonoBehaviour
+{
+    [SerializeField]
+    CanvasGroupFader _CanvasGroupFader;
+
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(2.0f);
+
+        yield return _CanvasGroupFader.FadeSceneOut();
+
+        yield return new WaitForSeconds(2.0f);
+
+        yield return _CanvasGroupFader.FadeSceneIn();
+    }
+
+}

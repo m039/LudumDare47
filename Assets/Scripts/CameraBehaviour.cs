631e0b2fb6d1c876d4bab65efa773da82a483108
Dmitry Mozgin <m0391n@gmail.com>
2020-10-03 20:32:41 +0300

    Fixed camera behaviour. It is now acceptable quality

--- Assets/Scripts/CameraBehaviour.cs
+++ Assets/Scripts/CameraBehaviour.cs
@@ -13,9 +13,22 @@ public class CameraBehaviour : MonoBehaviour
     [Header("Dependencies")]
     public PlayerBehaviour player;
 
+    public float moveSpeed = 2;
+
+    public float lookAtSpeed = 2f;
+
+    Vector3 _newPosition;
+
+    Vector3 _lookAtPosition;
+
     private void OnEnable()
     {
         UpdateCameraWithOffset();
+
+        if (targetToLock != null)
+        {
+            _lookAtPosition = targetToLock.transform.position;
+        }
     }
 
     private void OnValidate()
@@ -27,9 +40,35 @@ public class CameraBehaviour : MonoBehaviour
     {
         UpdateCameraWithOffset();
 
+        if (!Application.isPlaying)
+            return;
+
+        // Move to new position.
+        {
+            var delta = moveSpeed * Time.deltaTime;
+            var direction = (_newPosition - Camera.main.transform.position);
+            if (direction.magnitude > delta)
+            {
+                direction = direction.normalized * delta;
+            }
+
+            Camera.main.transform.position += direction;
+        }
+
+        // Lock to target.
+
         if (targetToLock != null)
         {
-            Camera.main.transform.LookAt(targetToLock);
+            var delta = lookAtSpeed * Time.deltaTime;
+            var direction = (targetToLock.position - _lookAtPosition);
+            if (direction.magnitude > delta)
+            {
+                direction = direction.normalized * delta;
+            }
+
+            _lookAtPosition += direction;
+
+            Camera.main.transform.LookAt(_lookAtPosition);
         }
     }
 
@@ -39,7 +78,14 @@ public class CameraBehaviour : MonoBehaviour
             return;
 
         var rotation = Quaternion.Euler(0, player.BulletRotation.eulerAngles.y, 0);
+        var newPosition = player.transform.position + rotation * offset;
 
-        transform.position = player.transform.position + rotation * offset;
+        if (Application.isPlaying)
+        {
+            _newPosition = newPosition;
+        } else
+        {
+            transform.position = newPosition;
+        }
     }
 }

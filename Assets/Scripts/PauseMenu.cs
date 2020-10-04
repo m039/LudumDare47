1b4d9d99dba39e566717c62be51ed319fba4faac
Dmitry Mozgin <m0391n@gmail.com>
2020-10-04 21:28:22 +0300

    Added rough paused menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public Transform canvas;
+
+    private void Awake()
+    {
+        canvas.gameObject.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            var isPaused = canvas.gameObject.activeSelf;
+
+            Time.timeScale = isPaused ? 1.0f : 0.0f;
+
+            canvas.gameObject.SetActive(!isPaused);
+        }
+    }
+
+    
+}
